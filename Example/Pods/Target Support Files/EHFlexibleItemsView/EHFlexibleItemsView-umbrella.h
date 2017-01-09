0f446589ca672e2442a77dadf7271a02bd4086bf
Eric Huang <WaterFlowsEast@gmail.com>
2017-01-09 14:14:45 +0800

    pod install for Example

--- Example/Pods/Target Support Files/EHFlexibleItemsView/EHFlexibleItemsView-umbrella.h
+++ Example/Pods/Target Support Files/EHFlexibleItemsView/EHFlexibleItemsView-umbrella.h
@@ -1,5 +1,8 @@
 #import <UIKit/UIKit.h>
 
+#import "EHFlexibleItemsSelectionView.h"
+#import "EHFlexibleItemsView.h"
+#import "EHFlexibleLayout.h"
 
 FOUNDATION_EXPORT double EHFlexibleItemsViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char EHFlexibleItemsViewVersionString[];
