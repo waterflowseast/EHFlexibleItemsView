fe2016c949df79804cc05028875c05570b9b3530
Eric Huang <WaterFlowsEast@gmail.com>
2017-01-09 14:03:36 +0800

    update podspec

---
+++ Example/Pods/Target Support Files/EHFlexibleItemsView/EHFlexibleItemsView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_EHFlexibleItemsView : NSObject
+@end
+@implementation PodsDummy_EHFlexibleItemsView
+@end
---
+++ Example/Pods/Target Support Files/EHFlexibleItemsView/EHFlexibleItemsView-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double EHFlexibleItemsViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char EHFlexibleItemsViewVersionString[];
+
